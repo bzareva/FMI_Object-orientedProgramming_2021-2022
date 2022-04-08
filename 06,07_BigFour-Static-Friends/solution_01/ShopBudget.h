810cb2241f7570748966b8d08ecbd86879983add
Blagovesta Zareva <46886255+bzareva@users.noreply.github.com>
2022-04-08 15:08:27 +0300

    Create ShopBudget.h

---
+++ 06,07_BigFour-Static-Friends/solution_01/ShopBudget.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef _SHOP_BUDGET_H_
+#define _SHOP_BUDGET_H_
+
+#include "Product.h"
+
+class ShopBudget {
+
+private:
+
+	static double budget;
+	int countOfGoods;
+	Product* goods;
+
+public:
+	
+	ShopBudget();
+	ShopBudget(int count, Product* shopGoods);
+	ShopBudget(const ShopBudget& rhs);
+	ShopBudget& operator=(const ShopBudget& rhs);
+	~ShopBudget();
+
+	bool sale(const Product& product);
+	double getBudget()const;
+
+private:
+
+	void copyShop(const ShopBudget& rhs);
+	void freeShop();
+
+};
+
+#endif //_SHOP_BUDGET_H_
